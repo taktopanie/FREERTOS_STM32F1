dd5d441e466abf222c08f61005fbd600e6017d01
taktopanie <maciejzwolan@gmail.com>
2024-06-27 16:45:12 +0200

    STM32F103 FREERTOS config setup

--- common/ThirdParty/FreeRTOS/__FreeRTOSConfig_TEMPLATE.h
+++ common/ThirdParty/FreeRTOS/__FreeRTOSConfig_TEMPLATE.h
@@ -51,27 +51,27 @@
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( SystemCoreClock )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES			( 8 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 15 * 1024 ) )
+#define configMAX_PRIORITIES			( 5 )
+#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 70 )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 7 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES				1
-#define configQUEUE_REGISTRY_SIZE		8
+#define configQUEUE_REGISTRY_SIZE		0
 #define configCHECK_FOR_STACK_OVERFLOW	0
-#define configUSE_RECURSIVE_MUTEXES		1
+#define configUSE_RECURSIVE_MUTEXES		0
 #define configUSE_MALLOC_FAILED_HOOK	0
 #define configUSE_APPLICATION_TASK_TAG	0
-#define configUSE_COUNTING_SEMAPHORES	1
+#define configUSE_COUNTING_SEMAPHORES	0
 #define configGENERATE_RUN_TIME_STATS	0
 
 
 /* Software timer definitions. */
 #define configUSE_TIMERS				1
 #define configTIMER_TASK_PRIORITY		( 2 )
-#define configTIMER_QUEUE_LENGTH		10
+#define configTIMER_QUEUE_LENGTH		5
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
@@ -124,7 +124,7 @@ standard names. */
 #define xPortSysTickHandler SysTick_Handler
 
 //DODANE PRZEZE MNIE
-#include "SEGGER_SYSVIEW_FreeRTOS.h"
+//#include "SEGGER_SYSVIEW_FreeRTOS.h"
 
 #endif /* FREERTOS_CONFIG_H */
 
