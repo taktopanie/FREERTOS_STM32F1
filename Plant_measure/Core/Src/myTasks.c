1cc6bdaee9346db43777efe5bf39ec39afd05f49
taktopanie <maciejzwolan@gmail.com>
2024-07-17 16:47:37 +0200

    2 x stable measurements working

--- Plant_measure/Core/Src/myTasks.c
+++ Plant_measure/Core/Src/myTasks.c
@@ -15,7 +15,7 @@ void GROUND_MEASURE_task(void* vParameters)
 
 	while(1)
 	{
-		vTaskDelay(pdMS_TO_TICKS(500));
+		vTaskDelay(pdMS_TO_TICKS(5000));
 		HAL_ADC_Start_DMA(&hadc1, (uint32_t *)ADC_VALUE, 2);
 	}
 }
@@ -26,6 +26,12 @@ void LCD_PRINT_task(void* vParameters)
 	uint16_t measure[2];
 	char meas_str_value [16];
 
+	uint16_t ADC_1_saved = 9999;
+	uint16_t ADC_2_saved = 9999;
+	int j;
+
+	lcd_clear();
+
 	while(1)
 	{
 		xTaskNotifyWait(0,0,&measure_addr,portMAX_DELAY);
@@ -35,21 +41,48 @@ void LCD_PRINT_task(void* vParameters)
 		measure[0] = *wsk;
 		measure[1] = *(wsk+1);
 
+		//FRIST ADC MEASUREMENT
+		if(ADC_1_saved != measure[0])
+		{
+			ADC_1_saved = measure[0];
+			lcd_send_command(LCD_RETURN_HOME);
 
-		lcd_clear(); // <<<<<< LCD CLEAR MAKES DISPLAY BLINKS
-		//TODO: CLEAR LEFT NUMBERS IF VALUE IS LOWER ==>> lcd_send_command(LCD_RETURN_HOME);
+			itoa(measure[0], meas_str_value, 10);
 
-		itoa(measure[0], meas_str_value, 10);
+			lcd_send_text("ADC_1: ");
 
-		lcd_send_text("ADC_1: ");
-		lcd_send_text(meas_str_value);
+			lcd_send_text(meas_str_value);
 
+			//CLEAR THE REST OF THE RESULT ON LCD
+			j = 0;
+			while(meas_str_value[j]!= '\0')j++;
 
-		itoa(measure[1], meas_str_value, 10);
+			for(int i = j; i < 4; i++)
+			{
+				lcd_send_data(0x20);
+			}
+		}
 
-		lcd_send_command(SET_DDRAM_ADDR|LCD_LINE2);
-		lcd_send_text("ADC_2: ");
-		lcd_send_text(meas_str_value);
+		//SECOND ADC MEASUREMENT
+		if(ADC_2_saved != measure[1])
+		{
+			ADC_2_saved = measure[1];
+
+			itoa(measure[1], meas_str_value, 10);
+
+			lcd_send_command(SET_DDRAM_ADDR|LCD_LINE2);
+			lcd_send_text("ADC_2: ");
+			lcd_send_text(meas_str_value);
+
+			//CLEAR THE REST OF THE RESULT ON LCD
+			j = 0;
+			while(meas_str_value[j]!= '\0')j++;
+
+			for(int i = j; i < 4; i++)
+			{
+				lcd_send_data(0x20);
+			}
+		}
 	}
 
 }
